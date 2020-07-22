bf73e0d74ec701bc935cbd529635712dce81aac2
Nick Zolotko <nzolotko@laika.com>
2020-07-22 11:47:01 -0700

    Replace mosyleServer Variable with better RegEx handling of AWK.

--- setupDEP.sh
+++ setupDEP.sh
@@ -9,15 +9,12 @@ fi
 
 ##Admin Supplied Variables
 # Enrollment Code to Device/User Group
-mosyleEnrollCode=""
-# Server URL to Your Business Assigned to you
-# From Mosyle
-mosyleServer="biz-####"
+mosyleEnrollCode="CHANGE"
 
 #Non User Configurable Variables
-hash=$(/usr/bin/curl -sI https://join.mosyle.com/${mosyleEnrollCode} | /usr/bin/awk '/Location/ {print substr($2,47,20)}')
+trueEnrollURL=$(/usr/bin/curl -sI https://join.mosyle.com/${mosyleEnrollCode} | /usr/bin/awk 'match($2, /^https:\/\/[^[:space:]]*\.mosyle\.com\/mdm\/enrollment\/\?c=[a-zA-Z0-9]*/) {print substr($2,RSTART,RLENGTH)}')
 mosyleProfile="/tmp/mosyleEnroll.mobileconfig"
 
-/usr/bin/curl -s 'https://'${mosyleServer}'.mosyle.com/mdm/enrollment/?c='${hash}'&self' -o ${mosyleProfile}
+/usr/bin/curl -s ${trueEnrollURL}'&self' -o ${mosyleProfile}
 
 /usr/bin/profiles install -path=${mosyleProfile}
